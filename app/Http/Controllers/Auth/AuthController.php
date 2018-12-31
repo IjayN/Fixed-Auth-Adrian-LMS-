11e85bd80bc2278f6085bc5528cf9e55dca38a11
mageto <mageto.denis@gmail.com>
2018-12-31 12:14:16 +0300

    auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -108,7 +108,7 @@ class AuthController extends Controller
         $check = User::where('email', $email)->value('active');
 
         if ($check === 0){
-            $userType = User::where('email', $email)->pluck('userType');
+            $user_type = User::where('email', $email)->pluck('user_type');
             return $this->prepareResult(1, $check, [$user_type],"Success");
         }else{
 
